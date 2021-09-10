5559e94cb065fb7f0310d3d00c3c7cca3dbe4ce8
Welldy Rosman <welldyrosman@gmail.com>
2021-09-10 22:59:14 +0700

    Update ApotekController.php

--- app/Http/Controllers/ApotekController.php
+++ app/Http/Controllers/ApotekController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Helpers\Tools;
+use App\Models\DetailResep;
+use App\Models\MedicalScreen;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Support\Facades\DB;
@@ -19,10 +21,16 @@ class ApotekController extends Controller
        DB::beginTransaction();
        try{
             $this->validate($request,[
-                "medical_id"=>"required",
+                "resep_id"=>"required",
+                "preorder.*.barang_id"=>"required",
             ]);
             $data=$request->all();
-            $id=$data["medical_id"];
+            $id=$data["resep_id"];
+            $items=$request->input("preorder");
+            foreach($items as $item){
+                //$resepdetail=DetailResep::
+            }
+
             Tools::MedChangeStatus($id,3,3,2,5);
             DB::commit();
             return Tools::MyResponse(true,"OK",null,200);
@@ -32,8 +40,9 @@ class ApotekController extends Controller
        }
     }
     public function gettodaylist(){
-        $med=DB::select("select m.*,CONCAT('MED-',m.poli_id,'-',LPAD(m.id,4,'0')) as kode_pasien,CONCAT('AKP',LPAD(p.id,4,'0')) as kode_pasien,p.nama from medical m
-        join pasiens p on m.pasien_id=p.id where cast(m.created_at as date)='$this->now' and m.status=4");
+        $med=DB::select("select r.*,CONCAT('MED-',LPAD(r.id,6,'0')) as kode_trans,p.nama,LPAD(m.id,4,'0')) as kode_pasien from resep r
+        left join medical m on r.medical_id=m.id
+        left join pasiens p on m.pasien_id=p.id where cast(r.created_at as date)='$this->now' and r.status=2");
         return Tools::MyResponse(true,"OK",$med,200);
     }
 }
