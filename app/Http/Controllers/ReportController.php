660f5c31f94d2b1c74f5cfc042e5191857f6df40
Welldy Rosman <welldyrosman@gmail.com>
2021-10-24 19:31:40 +0700

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -20,15 +20,18 @@ class ReportController extends Controller
             $sort=$request->input('sort');
             $cmd=Tools::GenFilterQueryStr($filter);
             $orderby=Tools::GenSortQueryStr($sort);
-            $sql=DB::select("with sumresep as(
-                select sum(rd.qty*rd.harga)as total,cast(r.created_at as date) as days from resep r
-                join resep_detail rd on r.id=rd.resep_id
-                group by cast(r.created_at as date))
-            ,summed as(
-                select sum(fee) as feeamt,cast(created_at as date) as med_days,count(id) as visit_qty from medical group by cast(created_at as date)
-            )
-            select r.total,m.feeamt,r.days,r.total+m.feeamt as grand_total,m.visit_qty from sumresep r
-            left join summed m on r.days=m.med_days where 1=1 $cmd $orderby $page");
+            $sql=DB::select("
+            with sumresep as(
+                    select sum(rd.qty*rd.harga)as total,cast(r.created_at as date) as days from resep r
+                    join resep_detail rd on r.id=rd.resep_id
+                    group by cast(r.created_at as date))
+                ,summed as(
+                    select sum(m.fee) as feeamt,cast(r.created_at as date) as med_days,count(r.id) as visit_qty
+                    from resep r left join
+                    medical m on r.medical_id=m.id group by cast(r.created_at as date)
+                )
+                select r.total,m.feeamt,r.days,r.total+m.feeamt as grand_total,m.visit_qty from sumresep r
+                left join summed m on r.days=m.med_days where 1=1 $cmd $orderby $page");
             return Tools::MyResponse(true,"OK",$sql,200);
 
         }catch(Exception $e){
