490eb0ae183e85d7310c868be0998de76d03215b
Welldy Rosman <welldyrosman@gmail.com>
2021-10-24 19:39:06 +0700

    Update ResepController.php

--- app/Http/Controllers/ResepController.php
+++ app/Http/Controllers/ResepController.php
@@ -26,10 +26,16 @@ class ResepController extends Controller
                 select sum(qty*harga) as total,resep_id from resep_detail group by resep_id
             )
             select r.*,m.fee,CONCAT('TRX',LPAD(r.id,6,'0')) as trans_kode,
+            case when r.medical_id is not null then p.nama
+            when r.medical_id is null and r.pasien_id is not null then p2.nama
+            else r.cust_nm end as nama,
+            CONCAT('AKP',LPAD(p.id,4,'0')) as kode_pasien,
             s.nama as nama_staff
             from resep r
             left join sumd dr on r.id=dr.resep_id
             left join medical m on r.medical_id=m.id
+            left join pasiens p on m.pasien_id=p.id
+            left join pasiens p2 on r.pasien_id=p2.id
             left join staff s on s.id=r.staff_id where 1=1 $cmd $orderby $page";
             $resep=DB::select($resepstr);
             return Tools::MyResponse(true,"OK",$resep,200);
