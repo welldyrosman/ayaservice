99f09bd28a5d4cca87365e6aea383335ebbeb5bc
Welldy Rosman <welldyrosman@gmail.com>
2021-11-04 14:16:09 +0700

    Update MedicalController.php

--- app/Http/Controllers/MedicalController.php
+++ app/Http/Controllers/MedicalController.php
@@ -199,9 +199,9 @@ class MedicalController extends Controller{
         left join dokter d on m.dokter_id=d.id
         join pasiens u on m.pasien_id=u.id
         where m.id=$id");
-        $resep=DB::select("select b.id,b.iscomposite,d.qty,d.unit,b.harga,b.nama,r.id as resep_id,d.ispreorder from resep r
+        $resep=DB::select("select b.id,b.iscomposite,d.qty,d.unit,b.harga,b.nama,r.id as resep_id,d.ispreorder,r.payamt.r.special from resep r
         left join resep_detail d on r.id=d.resep_id
-        join barang b on d.barang_id=b.id and kind=1
+        left join barang b on d.barang_id=b.id and kind=1
         where r.medical_id=$id");
         $labs=Labs::where('medical_id',$id)->first();
         $medical->form=$medicalform;
