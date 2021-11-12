0794e6fbeab9da4aa3e650dcfda8e6b9bc3ead66
Welldy Rosman <welldyrosman@gmail.com>
2021-11-13 00:03:05 +0700

    Update ScreeningController.php

--- app/Http/Controllers/ScreeningController.php
+++ app/Http/Controllers/ScreeningController.php
@@ -28,7 +28,7 @@ class ScreeningController extends Controller
     }
     private function getform($id){
         $data=DB::select("SELECT f.*,k.nama,k.datatype,f.id as medform_id,k.group_id FROM medform f
-        join medkind k on f.medkind_id=k.id where f.formkind_id=$id group by k.group_id asc
+        join medkind k on f.medkind_id=k.id where f.formkind_id=$id order by k.group_id asc
     ;");
         return $data;
     }
